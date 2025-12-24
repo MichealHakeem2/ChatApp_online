81b6804d362e941ec023de94ba45a8a19e65298d
MichealHakeem2 <michealhakeem33@gmail.com>
2025-12-24 14:05:30 +0200

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const db = process.env.MONGO_URI;
+// const db = process.env.MONGO_URI;
+const db = process.env.MONGO_URI_online_atlas;
 const connectDB = async () => {
     try {
         await mongoose.connect(db);
